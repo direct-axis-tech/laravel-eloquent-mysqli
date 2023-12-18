d2d79937396e7eef7f13f57053853aa8e64c2b16
Mohamed Mufeed <mufeedbinismail@gmail.com>
2023-12-18 18:08:59 +0400

    Bugfix: quote function missing in PDO

--- src/Wrappers/PDOWrapperForMysqli.php
+++ src/Wrappers/PDOWrapperForMysqli.php
@@ -2,6 +2,9 @@
 
 namespace LaravelEloquentMySQLi\Wrappers;
 
+use PDO;
+use RuntimeException;
+
 class PDOWrapperForMysqli {
     use WrapperTrait;
 
@@ -20,5 +23,22 @@ class PDOWrapperForMysqli {
     public function lastInsertId()
     {
         return $this->singleton->insert_id;
+    }
+
+    /**
+     * Polyfill for PDO quote
+     *
+     * @return int|string
+     */
+    public function quote($value, $type = PDO::PARAM_STR)
+    {
+        switch ($type) {
+            case PDO::PARAM_STR:
+                return "'{$this->singleton->real_escape_string($value)}'";
+            case PDO::PARAM_INT:
+                return (int) $value;
+            default:
+                throw new RuntimeException("Unhandled Type Passed To PDOWrapperForMysqli::quote", 1);
+        }
     }
 }
