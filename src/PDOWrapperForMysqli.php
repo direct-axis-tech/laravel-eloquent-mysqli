74c76359a1d97d04952a0aa96f9845ef1b3a5a45
Mohamed Mufeed <mufeedbinismail@gmail.com>
2025-12-17 10:21:09 +0400

    Added support for PDO like transaction methods

--- src/PDOWrapperForMysqli.php
+++ src/PDOWrapperForMysqli.php
@@ -4,6 +4,7 @@ namespace LaravelEloquentMySQLi;
 
 use mysqli;
 use PDO;
+use PDOException;
 use RuntimeException;
 
 class PDOWrapperForMysqli {
@@ -13,6 +14,13 @@ class PDOWrapperForMysqli {
      * @var \mysqli
      */
     protected mysqli $singleton;
+
+    /**
+     * Track whether we're currently in a transaction.
+     *
+     * @var bool
+     */
+    protected $inTransaction = false;
 
     public function __construct(mysqli $singleton)
     {
@@ -70,5 +78,86 @@ class PDOWrapperForMysqli {
             default:
                 throw new RuntimeException("Unhandled Type Passed To PDOWrapperForMysqli::quote", 1);
         }
+    }
+
+    /**
+     * Polyfill for PDO beginTransaction
+     *
+     * @return bool
+     * @throws PDOException
+     */
+    public function beginTransaction()
+    {
+        if ($this->inTransaction) {
+            throw new PDOException("There is already an active transaction");
+        }
+
+        if (!$this->singleton->autocommit(false)) {
+            throw new PDOException(
+                "Failed to begin transaction: {$this->singleton->error}",
+                $this->singleton->errno
+            );
+        }
+
+        $this->inTransaction = true;
+        return true;
+    }
+
+    /**
+     * Polyfill for PDO commit
+     *
+     * @return bool
+     * @throws PDOException
+     */
+    public function commit()
+    {
+        if (!$this->inTransaction) {
+            return false;
+        }
+
+        if (!$this->singleton->commit()) {
+            throw new PDOException(
+                "Failed to commit transaction: {$this->singleton->error}",
+                $this->singleton->errno
+            );
+        }
+
+        $this->singleton->autocommit(true);
+        $this->inTransaction = false;
+        return true;
+    }
+
+    /**
+     * Polyfill for PDO rollBack
+     *
+     * @return bool
+     * @throws PDOException
+     */
+    public function rollBack()
+    {
+        if (!$this->inTransaction) {
+            return false;
+        }
+
+        if (!$this->singleton->rollback()) {
+            throw new PDOException(
+                "Failed to rollback transaction: {$this->singleton->error}",
+                $this->singleton->errno
+            );
+        }
+
+        $this->singleton->autocommit(true);
+        $this->inTransaction = false;
+        return true;
+    }
+
+    /**
+     * Polyfill for PDO inTransaction
+     *
+     * @return bool
+     */
+    public function inTransaction()
+    {
+        return $this->inTransaction;
     }
 }
